9a7b5eaac77f60724ec2e8f1be36ad7dcc5b609f
pikachusugar <regs.sites.tats@gmail.com>
2021-06-04 22:09:05 +0900

    add sample using system-in into AnswerCodeSample

--- src/com/codetest/answers/sample/AnswerCodeSample.java
+++ src/com/codetest/answers/sample/AnswerCodeSample.java
@@ -1,10 +1,15 @@
 package com.codetest.answers.sample;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class AnswerCodeSample {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.println(Arrays.toString(args));
+		System.out.println(sc.nextLine());
+		
 		System.out.println("---write logic here---");
 	}
 }
